2f1e5de7f76858e485237afb72202776e02a27e9
FelipeColona <felipedev202@gmail.com>
2023-06-10 21:12:11 -0300

    feat: add uniform and accelerated motion

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <cstddef>
+#include <memory>
 #include <vector>
 #define OLC_PGE_APPLICATION
 #include "olcPixelGameEngine.h"
@@ -15,13 +16,92 @@ public:
 		// Name your application
 		sAppName = "Animation";
 	}
-    
+enum eShapes
+    {
+        SQUARE,
+        CIRCLE
+    };
+
+    struct sObject
+    {
+        float x;
+        float y;
+        float velocityX;
+        float velocityY;
+        float accelerationX;
+        float accelerationY;
+        eShapes shape;
+        int size;
+            
+        //Uniform Motion 
+        sObject(float x, float y, float velocityX, float velocityY, eShapes shape, int size)
+        {
+            this->x = x;
+            this->y = y;
+            
+            this->velocityX = velocityX;
+            this->velocityY = velocityY;
+
+            this->accelerationX = 0;
+            this->accelerationY = 0;
+
+            this->shape = shape;
+            this->size = size;
+        };
+            
+        //Accelerated Motion
+        sObject(float x, float y, float velocityX, float velocityY, float accelerationX, float accelerationY, eShapes shape, int size) 
+        {
+            this->x = x;
+            this->y = y;
+            
+            this->velocityX = velocityX;
+            this->velocityY = velocityY;
+            this->accelerationX = accelerationX;
+            this->accelerationY = accelerationY;
+            this->shape = shape;
+            this->size = size;
+        }
+    };
 private:
         
+    vector<sObject> vecSpaceObjects;
+        
+    void drawObjects(float fElapsedTime)
+    {
+        for (auto &object : vecSpaceObjects)
+        {
+            object.velocityX += object.accelerationX * fElapsedTime;
+            object.velocityY += object.accelerationY * fElapsedTime;
+            
+            object.x += object.velocityX * fElapsedTime;
+            object.y += -object.velocityY * fElapsedTime;
+
+            //If it is a square
+            if(object.shape == 0)
+            {
+                for(int x = 0; x < object.size; x++)
+                {
+                    for(int y = 0; y < object.size; y++)
+                    {
+                        Draw(object.x + x, object.y + y, olc::WHITE);
+                    }
+                };
+            }
+            else if(object.shape)
+            {
+                FillCircle(object.x, object.y, object.size, olc::WHITE);
+            }
+        };
+    }
 
 public:
 	bool OnUserCreate() override
 	{
+        //Oblique Throw
+        sObject square = sObject(0.0f, 235.0f, 125, 125, 0, -115, eShapes(1), (int) 16);
+        vecSpaceObjects.push_back(square);
+        
 		return true;
 	}
 
@@ -29,6 +109,7 @@ public:
 	{
         //clear screen
         FillRect(0, 0, ScreenWidth(), ScreenHeight(), olc::Pixel(0, 0, 0));
+        drawObjects(fElapsedTime);
 
         return true;
 	}
@@ -40,4 +121,4 @@ int main()
 	if (demo.Construct(256, 240, 4, 4))
 		demo.Start();
 	return 0;
-}
+}	
--- makefile
+++ makefile
@@ -1,3 +1,6 @@
 main:
 	g++ -o animation main.cpp -lX11 -lGL -lpthread -lpng -lstdc++fs -std=c++17
 	./animation
+debug:
+	g++ -g main.cpp -lX11 -lGL -lpthread -lpng -lstdc++fs -std=c++17
+	gdb a.out core.animation
